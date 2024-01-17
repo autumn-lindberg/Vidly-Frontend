cd6ebd064242c552590e1b3f239199ca14a4c95c
Autumn <78074803+autumnlindberg1@users.noreply.github.com>
2024-01-16 20:08:55 -0800

    fixed api docs display

--- src/components/movieDetails.jsx
+++ src/components/movieDetails.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Footer from "./footer";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import MovieForm from "./movieForm";
 
 const MovieDetails = () => {
